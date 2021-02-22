364c442832447163dfce4ad8edb9201be0dd387c
Wes Cossick <wes@sparksuite.com>
2021-02-22 13:25:35 -0600

    Push what I have

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-// Exports
-export { init, parse } from './waterfall-cli';
-export { Settings, CommandSpec } from './types';
-export { default as printError } from './print-pretty-error';
+// Imports
+import { init, parse } from './waterfall-cli';
+import printError from './print-pretty-error';
+
+// Export in a way that's compatible with ES6 and CommonJS
+const waterfall = {
+	init,
+	parse,
+	printError,
+};
+
+export = {
+	...waterfall,
+	default: waterfall,
+};
+
+// Expose some types
+export type { Settings, CommandSpec } from './types';
